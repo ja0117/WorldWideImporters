ef8135eb577b6dcadef2a61e3c999799dc8cd351
Robin-2002 <57712089+Robin-2002@users.noreply.github.com>
2019-11-13 14:02:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,72 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Page Title</title>
+    <title>Niks</title>
     <link rel="stylesheet" href="styles1.css">
 </head>
 <body>
 
-<div class="div1">[Intro]
-    Hé
-    Kom op, met z'n allen
-    Hé
-    Gezelligheid, kom op
-    Wooh
-    Eindhoven de gekste (hahaha)
-    Daar gaat die dan, met z'n allen
-    DoeMaarDave, hier ben ik dan (hahaha)
-    Yes
+<div class="div1">
 
-    [Chorus]
-    Jongedame zeg waar is het fuifje
-    Ik ben altijd te porren voor een fuifje
-    Je kan me vinden op alle leuke fuifjes
-    Ik geniet met volle teugen van die fuifjes
-
-    [Verse 1]
-    Vanavond is een knalfuif
-    Ik pak de gel en ik maak een kuif
-    Spreid mijn vleugels voel me net een duif (hoehoe, hoehoe)
-    Meisjes lachen als ik naar ze wuif (hahaha)
-    Jij bent een bloem ik bestuif je
-    Handen in de lucht voor het fuifje
-
-    [Chorus]
-    Jongedame zeg waar is het fuifje
-    Ik ben altijd te porren voor een fuifje
-    Je kan me vinden op alle leuke fuifjes
-    Ik geniet met volle teugen van die fuifjes
-
-    [Verse 2]
-    Vanavond ga ik erop uit (mwah)
-    Met een lach om m'n blanke snuit (hey)
-    DJ draai die liedjes lekker luid
-    Hopelijk vind ik mijn toekomstige bruid (ah)
-    Ik houd van fruit je bent mijn druifje
-    Handen in de lucht voor het fuifje
-
-    [Chorus]
-    Jongedame zeg waar is het fuifje
-    Ik ben altijd te porren voor een fuifje
-    Je kan me vinden op alle leuke fuifjes
-    Ik geniet met volle teugen van die fuifjes
-
-    [Bridge]
-    Lalalala Lalalalalala
-    Lalalala Lalalalalala
-    Lalalala Lalalalalala
-    Handen in de lucht voor dat fuifje
-
-    [Chorus]
-    Jongedame zeg waar is het fuifje
-    Ik ben altijd te porren voor een fuifje
-    Je kan me vinden op alle leuke fuifjes
-    Ik geniet met volle teugen van die fuifjes
-
-    [Outro]
-    Fuifjes, fuifjes, fuifjes, fuifjes, fuifjes, fuifjes
-    Ik houd van fuifjes, yeah
-    Ik geniet met volle teugen van die fuifjes</div>
+</div>
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
 
