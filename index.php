2d1430421d02c548d9724c02d5244b3cc47d92a4
PAhrary <pamirahrary@gmail.com>
2019-11-13 13:48:30 +0100

    css toegevoegd

--- index.php
+++ index.php
@@ -3,3 +3,19 @@ echo "test<br>";
 echo "test 2<br>";
 echo "jij bent een bloem ik bestuif je";
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="styles1.css">
+</head>
+<body>
+
+<div class="div1">ik hou van fruit jij bent mijn druifje</div>
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
+
