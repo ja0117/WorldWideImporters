586e761cfe6ec5b13e535e934fbfb5d3f0906101
Joshua <joshua007.joshua@gmail.com>
2019-11-17 21:02:21 +0100

    basic navbar, searchbar, layout

--- index.php
+++ index.php
@@ -5,15 +5,28 @@ include_once("databasecon.php");
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style/indexStyle.css">
     <title>World Wide Importers</title>
-    <link rel="stylesheet" href="styles1.css">
 </head>
 <body>
+<!-- Top navigation bar -->
+<div class="topnav">
+    <a class="active" href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
+    <div id="search">
+        <input type="text" name="searchBox" placeholder="search"> <input type="button" value="search" name="searchBtn">
+    </div>
+</div>
+
 
 <header> <h1>Welkom bij WWI!</h1> </header>
 
 <div>
-    <?php
+    <!--
+
     $sql = 'SELECT * FROM customers';
 
     foreach ($conn->query($sql) as $row) {
@@ -21,10 +34,11 @@ include_once("databasecon.php");
         print($row["CustomerName"] . "\t");
         print("<br>");
     }
-    ?>
+
+    -->
 </div>
 
-<footer> Copyright &copy; 2019, World Wide Importers</footer>
+<footer> Copyright &copy; 2019, Wide World Importers</footer>
 
 </body>
 </html>
