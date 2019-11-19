b94caec1008fefe540efa0c00f0bea8a9bec99d2
Aziz <aziiz.abdulahi@gmail.com>
2019-11-19 11:23:03 +0100

    Files

--- Aziztest/includes/head.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style/indexStyle.css">
-    <title>World Wide Importers</title>
-</head>
--- databasecon.php
+++ databasecon.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $database = "wideworldimporters";
 
 // Create connection
@@ -11,5 +11,5 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected!";
+
 
