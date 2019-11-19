dcb26d68eec425a16de7b146770940fac89354ef
angelozzi <57720401+angelozzi@users.noreply.github.com>
2019-11-19 23:47:40 +0100

    productpagina

---
+++ productpagina.php
@@ -0,0 +1,69 @@
+<?php
+include_once("databasecon.php");
+
+
+if (isset($_GET["test"])) {
+    $statement = mysqli_prepare($conn, "SELECT * FROM stockitems WHERE StockItemID = ?");
+
+    mysqli_stmt_bind_param($statement, 'i', $_GET["test"]);
+    mysqli_stmt_execute($statement);
+    $result = mysqli_stmt_get_result($statement);
+
+    if (isset($_GET["test"])) {
+        if (mysqli_num_rows($result) > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $name = $row["StockItemName"];
+                $price = $row["UnitPrice"];
+                $description = $row["MarketingComments"];
+                $btw = 1 + $row["TaxRate"] / 100;
+            }
+        }
+    }
+
+}
+else{
+    print("Product niet gevonden!");
+}
+?>
+<!DOCTYPE html>
+<html>
+<!-- HTML head -->
+<?php include 'includes/head.php'; ?>
+
+<!-- Header & Nav bar -->
+<?php include 'includes/headernav.php'; ?>
+<head>
+    <link rel="stylesheet" href="productpage.css">
+</head>
+
+<body>
+
+<div class="image">
+    <img src="images/img_avatar.jpg" alt="Avatar" style="width:100%">
+
+</div>
+
+<div class="naam">
+<?php
+print($name)
+?>
+</div>
+
+<div class="prijs">
+    <?php
+    print(  "€" .$price * $btw)
+    ?>
+</div>
+
+<div class="desc">
+    <?php
+    print($description)
+    ?>
+</div>
+
+<button type="button" onclick="alert('dit is helaas nog niet af')">Add to cart</button>
+
+</body>
+
+<?php include 'includes/footer.php'; ?>
+</html>
