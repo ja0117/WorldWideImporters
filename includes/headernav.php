7b18ad2bcb398e38a80ff78754dcae3049841438
Joshua <joshua007.joshua@gmail.com>
2019-11-19 22:57:08 +0100

    us3.php heet nu search.php

--- includes/headernav.php
+++ includes/headernav.php
@@ -3,7 +3,7 @@
         <a class="active" href="index.php">Home</a>
         <a href="categories.php">Categories</a>
         <a href="#about">About</a>
-        <form id="search" action="us3.php">
+        <form id="search" action="search.php">
             <input type="text" name="searchBox" placeholder="Search for anything" id="searchBox">
             <input type="submit" value="Search" name="searchButton" id="searchButton">
         </form>
