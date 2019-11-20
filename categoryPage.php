dd984b2eb8ed38ad9495ba89424106d551250720
Aziz <aziiz.abdulahi@gmail.com>
2019-11-20 14:26:00 +0100

    Update categoryPage.php

--- categoryPage.php
+++ categoryPage.php
@@ -21,8 +21,8 @@
     <option value= "25" <?php if ($_POST['load'] == "25") echo 'selected="selected" '; ?>>25</option>
     <option value="50" <?php if ($_POST['load'] == "50") echo 'selected="selected" '; ?>>50</option>
     <option value="100" <?php if ($_POST['load'] == "100") echo 'selected="selected" '; ?>>100</option>
-    <noscript><input type="submit" value="Submit"></noscript>
   </select>
+  <noscript><input type="submit" value="Submit"></noscript>
 </form>
 
 
