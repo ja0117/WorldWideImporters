761c7685a99c715766dd2aacc0dc396b4bf51184
Robin-2002 <57712089+Robin-2002@users.noreply.github.com>
2019-11-20 12:41:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,17 +13,21 @@
 
 <div>
     <h1> Welkom bij WWI!</h1>
-    <!--
 
-    $sql = 'SELECT * FROM customers';
+    <?php
+    $statement = mysqli_prepare($conn, "SELECT StockItemName, RecommendedRetailPrice, COUNT(T.StockItemID) FROM stockitems S JOIN stockitemtransactions T ON S.StockItemID = T.StockItemID  GROUP BY T.StockItemID ORDER BY COUNT(T.StockItemID) desc LIMIT 5");
+    mysqli_stmt_execute($statement);
+    $result = mysqli_stmt_get_result($statement);
+    foreach ($result as $row) {
+        ?>
+        <div class="card"?>
+        <img style="Width: 250px; height: 250px;" src="images/<?php print substr($row["StockItemName"], 0, 3) ?>.jpg">
+        <div class="container">
+            <h4><b><?php print($row["StockItemName"]); ?></b></h4>
+            <p><?php print("Prijs: ".$row["RecommendedRetailPrice"]); ?></p>
+        </div>
+        </div><?php } ?>
 
-    foreach ($conn->query($sql) as $row) {
-        print($row["CustomerID"] . "\t");
-        print($row["CustomerName"] . "\t");
-        print("<br>");
-    }
-
-    -->
 </div>
 
 </body>
