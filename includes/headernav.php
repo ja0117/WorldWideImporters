4ae9f0baa81d8c35b2e706c55c81de0ba6a38e79
angelozzi <57720401+angelozzi@users.noreply.github.com>
2019-11-20 11:36:13 +0100

    zoekbalk
    prachtig ding

--- includes/headernav.php
+++ includes/headernav.php
@@ -1,11 +1,12 @@
 <header>
     <div class="topnav">
-        <a class="active" href="index.php">Home</a>
-        <a href="categories.php">Categories</a>
-        <a href="#about">About</a>
-        <form id="search" action="search.php">
+        <a class="home" href="index.php"><img class="home" src="images/wwilogo.png" alt="Logo"></a>
+        <a class="click" href="categories.php">Categories</a>
+        <a class="click" href="#about">About</a>
+        <a class="click" href="#search">Mijn Account</a>
+        <form id="search" action="us3.php">
             <input type="text" name="searchBox" placeholder="Search for anything" id="searchBox">
-            <input type="submit" value="Search" name="searchButton" id="searchButton">
+            <input type="image" src="images/searchbutton.png" height="20px"; width="20px"; name="searchButton" id="searchButton">
         </form>
     </div>
 </header>
