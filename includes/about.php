03d77e1d8858c6b73df545903ed35b13e254dac4
Robin-2002 <57712089+Robin-2002@users.noreply.github.com>
2019-11-23 12:00:55 +0100

    Header aangepast + begonnen met de over ons pagina(die lukt niet)

---
+++ includes/about.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<!-- HTML head -->
+<?php include 'includes/searchHead.html'; ?>
+
+<!-- Header & Nav bar -->
+<?php include 'includes/headernav.php'; ?>
+
+<body>
+
+<div><h2> Over ons </h2></div>
+
+<div>
+</div>
+
+<div>
+</div>
+
+<div>
+</div>
+
+</body>
+
+<!-- Footer -->
+<?php include 'includes/footer.php'; ?>
+
+</html>
