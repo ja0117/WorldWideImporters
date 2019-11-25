f0e062442af9a21aed424d0643151238ee2a05d3
PAhrary <pamirahrary@gmail.com>
2019-11-25 14:45:06 +0100

    Update shoppingcart.php

--- shoppingcart.php
+++ shoppingcart.php
@@ -1,8 +1,4 @@
-<?php
-
-
-
-?>
+<?php include_once("databasecon.php"); ?>
 
 <!DOCTYPE html>
 <html>
@@ -18,6 +14,35 @@
 
 <body>
 
+<div class="page-container">
+
+    <?php $products = "SELECT Product.StockItemID, StockItemName, RecommendedRetailPrice
+    FROM stockitems Product
+    JOIN stockitemstockgroups Cat ON Product.StockItemID = Cat.StockItemID
+    LIMIT 4";
+
+    $result = mysqli_query($conn, $products);
+
+    foreach ($result as $row) { ?>
+    <div class="card">
+        <form method="post" action="shoppingcart.php?action=add&id="<?php print $row["StockItemID"]; ?> >
+            <div>
+                <img style="width:250px; height:250px" src="images/<?php print substr($row["StockItemName"], 0, 3) ?>.jpg">
+                <div class="container">
+                    <h4><b><?= $row["StockItemName"]; ?></b></h4>
+                    <div id="itemPrice">
+                        <?php print("&#8364;" . $row["RecommendedRetailPrice"] . ",-"); ?>
+                    </div>
+                </div>
+            </div>
+            <input type="hidden" name="hidden_name" value="<?php print $row["StockItemName"]; ?>" >
+            <input type="hidden" name="hidden_price" value="<?php print $row["RecommendedRetailPrice"]; ?>" >
+            <input type="submit" name="add_to_cart" value="Voeg toe aan winkelmand">
+        </form>
+    </div>
+    <?php }; ?>
+
+</div>
 
 </body>
 
