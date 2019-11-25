38fa923281650d04a82e228111540c9122a3c91c
PAhrary <pamirahrary@gmail.com>
2019-11-25 12:35:34 +0100

    shoppingcart toegevoegd

--- includes/headernav.php
+++ includes/headernav.php
@@ -4,6 +4,7 @@
         <a class="click" href="categories.php">Categorieën</a>
         <a class="click" href="about.php">Over ons</a>
         <a class="click" href="#search">Mijn account</a>
+        <a href="shoppingcart.php"> <img class="shoppingCart" src="images/shoppingcart.jpg" style="height:30px; width:50px;"> </a>
         <form id="search" action="search.php">
             <input type="text" name="searchBox" class="rounded-corners-right" placeholder="Zoek hier uw producten..." id="searchBox">
             <input type=image id="searchButton" class="rounded-corners-left" src="images/mglas.jpg">
