040618ff47f9347a53d3a8a7bb83e2de6d22a8a4
Vadimpje <Vladgam3x@gmail.com>
2019-11-28 09:26:43 +0100

    Update categoryPage.php

--- categoryPage.php
+++ categoryPage.php
@@ -18,9 +18,9 @@
 <form method="post">
 <!--The PHP compares each option with the current display amount setting and displays the corresponding one as selected  -->
   <select class="select-css" name="load" onchange='this.form.submit()'>>
-    <option value= "25" <?php if ($_POST['load'] == "25") echo 'selected="selected" '; ?>>25</option>
+    <option value= "25" <?php if ($_POST['load'] == "25") {echo 'selected="selected" '; ?>>25</option>
     <option value="50" <?php if ($_POST['load'] == "50") echo 'selected="selected" '; ?>>50</option>
-    <option value="100" <?php if ($_POST['load'] == "100") echo 'selected="selected" '; ?>>100</option>
+    <option value="100" <?php if ($_POST['load'] == "100") echo 'selected="selected" '; }?>>100</option>
   </select>
 <!--All the javascript does is wait until an option has been selected and submits the form when it is -->
   <noscript><input type="submit" value="Submit"></noscript>
