8fa2d401c51a3b825ac214ab1ec4a67ea5860294
PAhrary <pamirahrary@gmail.com>
2019-12-02 11:28:27 +0100

    shoppingcart functionality done

--- shoppingcart.php
+++ shoppingcart.php
@@ -9,22 +9,24 @@ if (empty($_SESSION["shoppingCart"])) {
     $_SESSION["shoppingCart"] = array();
 }
 
+// If "+" button is pressed, make an array $itemdata inside the shoppingCart session
 if (isset($_POST["add_to_cart"])) {
     $itemdata = array (
         "item_productid"    =>    $_POST["hidden_productid"],
         "item_productname"  =>    $_POST["hidden_productname"],
         "item_quantity"     =>    $_POST["quantity"],
-        "item_productprice" =>    $_POST["hidden_productprice"] * $_POST["quantity"]
+        "item_productprice" =>    $_POST["hidden_productprice"]
     );
     $exists=false;
 
+// If shoppingCart is empty, push the item data inside the array, else add up the specified quantity
     if (empty($_SESSION["shoppingCart"])) {
         array_push($_SESSION["shoppingCart"], $itemdata);
         $exists=true;
     }  else {
         foreach ($_SESSION["shoppingCart"] as $index => $values) {
             if ($values["item_productid"] === $_POST["hidden_productid"]) {
-                $values["item_quantity"] = $_POST["quantity"] + $values["item_quantity"];
+                $values["item_quantity"] += $_POST["quantity"];
                 $_SESSION["shoppingCart"][$index]=$values;
                 $exists = true;
                 break;
@@ -37,8 +39,22 @@ if (isset($_POST["add_to_cart"])) {
     }
 }
 
-
+// If "-" button is pressed, remove by the specified quantity. If quantity gets below 1, remove the item from shoppingCart
 if (isset($_POST["remove_from_cart"])) {
+    foreach ($_SESSION["shoppingCart"] as $index => $values) {
+        if ($values["item_productid"] === $_POST["hidden_productid"]) {
+            if ($values["item_quantity"] > 1) {
+                $values["item_quantity"] -= $_POST["quantity"];
+                $_SESSION["shoppingCart"][$index] = $values;
+            } else {
+                unset($_SESSION["shoppingCart"][$index]);
+            }
+        }
+    }
+}
+
+// If "Remove" button is pressed, remove item completely from shoppingCart
+if (isset($_POST["remove"])) {
     foreach ($_SESSION["shoppingCart"] as $index => $values) {
         if ($values["item_productid"] === $_POST["hidden_productid"]) {
             unset($_SESSION["shoppingCart"][$index]);
@@ -47,7 +63,7 @@ if (isset($_POST["remove_from_cart"])) {
 }
 
 print_r($_SESSION["shoppingCart"]);
-//          unset($_SESSION["shoppingCart"]);
+
 ?>
 
 <!DOCTYPE html>
@@ -93,6 +109,8 @@ print_r($_SESSION["shoppingCart"]);
                 <input type="hidden" name="hidden_productprice" value="<?php print $row["UnitPrice"]; ?>">
                 <input type="submit" name="add_to_cart" value="+" >
                 <input type="submit" name="remove_from_cart" value="-" >
+                <input type="submit" name="remove" value="Remove">
+
             </form>
         </div>
     <?php }; ?>
