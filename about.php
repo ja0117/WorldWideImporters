c16b2a40994067f871bb513eae2acb87dc3f4548
angelozzi <57720401+angelozzi@users.noreply.github.com>
2019-12-05 01:06:58 +0100

    zodat we niet weer een "IK WIL NU ACCOUNT" situatie krijgen

--- about.php
+++ about.php
@@ -10,7 +10,9 @@
 <body>
 
 <div><h2> Over ons </h2></div>
-
+<div style="text-align: center">
+<img src="https://transfixit.nl/wp-content/uploads/2019/03/website-under-construction-landing-page-vector-20519213.jpg" alt="Under Construction">
+</div>
 <div>
 </div>
 
