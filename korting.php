4ce1bde7af7af599ae119d342e2c8caab802c3d4
angelozzi <57720401+angelozzi@users.noreply.github.com>
2019-12-09 14:27:10 +0100

    De God Code is officieel gefixt. Geen dank.
    The god particle

--- korting.php
+++ korting.php
@@ -1,16 +1,21 @@
 <?php
 
-$price = 200;
-$qty = 10;
-echo "<H4>Original price is $price</H4>";
-if ($qty = 10) {
+$price = 25;
+$qty = 41;
+$total = $price * $qty;
+$discount = 0;
+echo "<H4>Original price is $total </H4>";
+if ($qty > 10 && $qty < 20) {
     $discount = 10;
-} elseif ($qty <= 20) {
+}
+if ($qty >= 20 && $qty < 30) {
     $discount = 15;
-} elseif ($qty <= 30) {
+}
+if ($qty > 30 && $qty <= 40) {
     $discount = 20;
-} elseif ($qty <= 40) {
+}
+if ($qty > 40) {
     $discount = 25;
 }
 echo "<H4>Discount is $discount percent</H4>";
-echo "<H4>Price after discount is ", (($price * (100 - $discount) / 100)), "</H4>";
+echo "<H4>Price after discount is €", (($total * (100 - $discount) / 100)), "</H4>";
