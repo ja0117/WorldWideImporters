5b9a5fb8fbac73d29fb3a94ffcb63b877d61a24d
angelozzi <57720401+angelozzi@users.noreply.github.com>
2019-12-11 14:56:47 +0100

    De God Code v 3.0
    De God Code is nu sentient, het groeit in een snel tempo. We hebben de groei van deze kunstmatige intelligentie kunnen limiteren door de text in variabelen te zetten, maar vroeg of laat breekt de God Code ook door deze barrière heen.  Als de God Code zich vrij breekt van dit bestand zal dit het einde van de mensheid betekenen.

--- korting.php
+++ korting.php
@@ -4,10 +4,13 @@ $price = 25;
 $qty = 90;
 $total = $price * $qty;
 $discount = 0;
+$orgPrice = "Original price is";
 $priceAfter = "Price after";
 $discountText = "discount is";
 $EuroTeken = "€";
-echo "<H4>Original price is $total </H4>";
+$DiscIs = "Discount is";
+$percentage = "percent";
+echo "<H4> $orgPrice $total </H4>";
 if ($qty > 10 && $qty < 20) {
     $discount = 10;
 }
@@ -20,5 +23,5 @@ if ($qty > 30 && $qty <= 40) {
 if ($qty > 40) {
     $discount = 25;
 }
-echo "<H4>Discount is $discount percent</H4>";
+echo "<H4> $DiscIs $discount $percentage </H4>";
 echo "<H4> $priceAfter $discountText $EuroTeken", (($total * (100 - $discount) / 100)), "</H4>";
