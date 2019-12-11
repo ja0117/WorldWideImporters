1ec50b10eb85404878183648748b87264470b52b
Aziz <aziiz.abdulahi@gmail.com>
2019-12-11 16:06:22 +0100

    Shoppingcart opmaak veranderd

--- shoppingcart.php
+++ shoppingcart.php
@@ -32,7 +32,9 @@ include("shoppingCartCode.php");
                         <tr>
                             <th scope="col"> </th>
                             <th scope="col">Product</th>
+                            <th></th>
                             <th scope="col" class="text-center">Aantal</th>
+                            <th></th>
                             <th scope="col" class="text-right">Prijs</th>
                             <th> </th>
                         </tr>
@@ -48,10 +50,11 @@ include("shoppingCartCode.php");
                         <input type="hidden" name="hidden_productid" value="<?= $values["item_productid"] ?>">
                             <td><img src="https://dummyimage.com/50x50/55595c/fff" /> </td>
                             <td><?= $values["item_productname"] ?></td>
-                            <td><input class="form-control" type="text" value="<?=$values["item_quantity"]?>" /></td>
-                            <td> <button type ="submit" name ="increaseQuantity" class="btn btn-succes"> <i class="fas fa-plus"> </i> </button> </td>
+                            <td> <button type ="submit" name ="increaseQuantity" class="btn btn-succes"> <i class="fas fa-plus-square"> </i> </button> </td>
+                            <td><?=$values["item_quantity"]?></td>
+                            <td> <button type ="submit" name ="decreaseQuantity" class="btn btn-succes"> <i class="fas fa-minus-square"> </i> </button> </td>
+
                             <td class="text-right">€<?= $values["item_productprice"] ?></td>
-                            <td> <button type ="submit" name ="decreaseQuantity" class="btn btn-succes"> <i class="fas fa-minus"> </i> </button> </td>
                             <td class="text-right"><button type ="submit" name="remove" class="btn btn-sm btn-danger"> <i class="fa fa-trash"> </i> </button> </td>
                         </tr>
                         </form>
