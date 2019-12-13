724c412f42cc0442025e529973a8aaf2b62d53a3
Robin-2002 <57712089+Robin-2002@users.noreply.github.com>
2019-12-13 14:29:49 +0100

    Ya yeet naar huis joshua??

--- index.php
+++ index.php
@@ -37,14 +37,13 @@
                 ?>
                 <!--A link to its own page is created based on its ID      -->
                 <div class="col-lg-4 col-md-6 mb-4">
-                <div class="card h-100">
+                <div class="card">
                 <a href="productpagina.php?product=<?php print($row['StockItemID']); ?>"><img class="card-img-top" src="images/<?php print substr(str_replace('"', '',$row["StockItemName"]), 0, 3) ?>.jpg" alt=""></a>
                 <div class="card-body">
-                <h4 class="card-title">
+                <p6 class="card-title">
                 <a href="productpagina.php?product=<?php print($row['StockItemID']); ?>"><?= $row["StockItemName"] ?></a>
-            </h4>
+            </p6>
             <h5><?= "€" . number_format((float) $row["UnitPrice"] * $btw , 2, ',', '') ?></h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
 
               </div>
               <div class="card-footer">
@@ -53,13 +52,46 @@
                       <input type="hidden" name="hidden_productname" value="<?= str_replace('"', '',$row['StockItemName']) ?>">
                       <input type="hidden" name="hidden_productprice" value="<?= $row["UnitPrice"] * $btw ?>">
                       <input type="hidden" name="quantity" value="1">
-                      <input type="submit" name="add_to_cart" class="btn btn-primary" value="Toevoegen aan winkelmand">
+                      <center><button type="submit" name="add_to_cart" class="btn btn-primary"><i class="fas fa-shopping-cart"></i></button></center>
                   </form>
               </div>
             </div>
           </div>
 
 <?php } ?>
+            <?php
+            $statement = mysqli_prepare($conn, "SELECT T.StockItemID, StockItemName, DiscountPercentage, TaxRate, S.UnitPrice
+            FROM stockitems S JOIN specialdeals T ON S.StockItemID = T.StockItemID LIMIT 9");
+            mysqli_stmt_execute($statement);
+            $result = mysqli_stmt_get_result($statement);
+            foreach ($result as $row) {
+                $btw = $row["TaxRate"] / 100 + 1;
+                ?>
+                <!--A link to its own page is created based on its ID      -->
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card h-100">
+                        <a href="productpagina.php?product=<?php print($row['StockItemID']); ?>"><img class="card-img-top" src="images/<?php print substr(str_replace('"', '',$row["StockItemName"]), 0, 3) ?>.jpg" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                <a href="productpagina.php?product=<?php print($row['StockItemID']); ?>"><?= $row["StockItemName"] ?></a>
+                            </h4>
+                            <h5><?= "€" . number_format((float) ($row["S.UnitPrice"] * $btw), 2, ',', '') ?></h5>
+                            <h4><?= "€" . number_format((float) ($row["S.UnitPrice"] * $btw)*($row["DiscountPercentage"]/100+1), 2, ',', '') ?></h4>
+
+                        </div>
+                        <div class="card-footer">
+                            <form method="post">
+                                <input type="hidden" name="hidden_productid" value="<?= $row['StockItemID']; ?>">
+                                <input type="hidden" name="hidden_productname" value="<?= str_replace('"', '',$row['StockItemName']) ?>">
+                                <input type="hidden" name="hidden_productprice" value="<?= $row["UnitPrice"] * $btw ?>">
+                                <input type="hidden" name="quantity" value="1">
+                                <input type="submit" name="add_to_cart" class="btn btn-primary" value="Toevoegen aan winkelmand">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            <?php } ?>
 
           </div>
       </div>
