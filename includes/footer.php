14657cb0bc8b4c60c9dd17a3e659b628c87cecee
PAhrary <pamirahrary@gmail.com>
2019-12-16 14:24:15 +0100

    klantenservice begin

--- includes/footer.php
+++ includes/footer.php
@@ -2,8 +2,16 @@
 
     <footer class="py-3 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">WWI &copy; Webshop 2019</p>
+        <div class="row">
+            <div class="col">
+            </div>
+            <div class="col">
+                <p class="m-0 text-center text-white">WWI &copy; Webshop 2019</p>
+            </div>
+            <div class="col">
+                <p class="m-0 text-center"> <a href="klantenservice.php" class="text-white">Klantenservice</a></p>
+            </div>
+        </div>
     </div>
     <!-- /.container -->
   </footer>
-
---
+++ klantenservice.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<!-- HTML head -->
+<?php include 'includes/head.php'; ?>
+<?php include 'includes/nav.php'; ?>
+
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="my-4"> Klantenservice </h1>
+        </div>
+    </div>
+</div>
+
+</body>
+
+<!-- Footer -->
+<?php include 'includes/footer.php'; ?>
+
+</html>
